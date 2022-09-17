edaaefca3789691b4f86ec546631bb2235c30578
Safiul-Azam <safiulazamriad@gmail.com>
2022-09-17 13:56:25 +0600

    product filter with size and category

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,42 +1,77 @@
 import React from 'react';
 import './navbar.css'
-import {IoRefresh} from 'react-icons/io5'
+import { IoRefresh } from 'react-icons/io5'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Products from '../Products/Products';
 
 const Navbar = () => {
+    const [size, setSize] = useState(undefined)
+    const [category, setCategory] = useState(undefined)
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch(`/products.json`)
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    const productsBySize = products.filter(product => product.size === size && product.category === category)
     const handleChange = () => {
 
     }
     return (
-        <nav className='nav-container'>
-            <div className='select-item'>
-                <select onChange={handleChange} id="adult">
-                    <option>- Hoodies</option>
-                    <option>Hoodies 1</option>
-                    <option>Hoodies 2</option>
-                    <option>Hoodies 3</option>
-                    <option>Hoodies 4</option>
-                    <option>Hoodies 5</option>
-                </select>
-                <select onChange={handleChange} id="adult">
-                    <option>Size</option>
-                    <option>sm</option>
-                    <option>md</option>
-                    <option>lg</option>
-                    <option>xl</option>
-                    <option>xll</option>
-                </select>
-                <p className='reset'><IoRefresh/> Reset</p>
-            </div>
-            <div className='search'>
-                <div className="search-products">
-                    <label className="label">
-                        <span className="label-text">Search : </span>
-                    </label>
-                    <input type="text" placeholder="Search" className="" />
+        <div>
+            <nav className='nav-container'>
+                <div className='select-item'>
+                    <select onChange={e => setCategory(e.target.value)} id="adult">
+                        <option>- category</option>
+                        <option>Bag</option>
+                        <option>Cap</option>
+                        <option>watch</option>
+                        <option>glass</option>
+                    </select>
+                    <select onChange={e => setSize(e.target.value)} id="adult">
+                        <option>Size</option>
+                        <option>S</option>
+                        <option>M</option>
+                        <option>L</option>
+                        <option>XL</option>
+                        <option>2XL</option>
+                    </select>
+                    <p className='reset'><IoRefresh /> Reset</p>
                 </div>
-                <button>Add To Cart</button>
+                <div className='search'>
+                    <div className="search-products">
+                        <label className="label">
+                            <span className="label-text">Search : </span>
+                        </label>
+                        <input type="text" placeholder="Search" className="" />
+                    </div>
+                    <button>Add To Cart</button>
+                </div>
+            </nav>
+            <div className="product-container">
+                <table className="">
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>color</th>
+                            <th>Stock</th>
+                            <th>Category</th>
+                            <th>Size</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th style={{ textAlign: 'right', paddingRight: '15px' }}>Buy</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            <Products products={productsBySize.length > 0? productsBySize: products }></Products>
+                        }
+                    </tbody>
+                </table>
             </div>
-        </nav>
+        </div>
     );
 };
 
--- src/component/Products/Products.js
+++ src/component/Products/Products.js
@@ -1,58 +1,34 @@
 import React from 'react';
 import './Products.css'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
-import { useState } from 'react';
-import { useEffect } from 'react';
 
-const Products = () => {
-    const [products, setProducts] = useState([])
-    useEffect(() => {
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    }, [])
+const Products = ({ products, productsData }) => {
     console.log(products);
+    console.log(productsData);
     return (
-        <div className="product-container">
-            <table className="">
-                <thead>
-                    <tr>
-                        <th>Image</th>
-                        <th>Name</th>
-                        <th>color</th>
-                        <th>Stock</th>
-                        <th>Category</th>
-                        <th>Size</th>
-                        <th>Quantity</th>
-                        <th>Price</th>
-                        <th style={{ textAlign: 'right', paddingRight: '15px' }}>Buy</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        products.map(product => <tr>
-                            <td>
-                                <img width={70} src={product.img} alt="" />
-                            </td>
-                            <td>{product.name}</td>
-                            <td>{product.color}</td>
-                            <td>{product.stock}</td>
-                            <td>{product.category}</td>
-                            <td>{product.size}</td>
-                            <td>{product.quantity}</td>
-                            <td>{product.price}</td>
-                            <td>
-                                <div className='check-cart'>
-                                    <input type="number" name="" id="" />
-                                    <AiOutlineShoppingCart className='cart' />
-                                    <input type="checkbox" name="" id="" />
-                                </div>
-                            </td>
-                        </tr>)
-                    }
-                </tbody>
-            </table>
-        </div>
+        <>
+            {
+                products?.map(product => <tr>
+                    <td>
+                        <img width={70} src={product.img} alt="" />
+                    </td>
+                    <td>{product.name}</td>
+                    <td>{product.color}</td>
+                    <td>{product.stock}</td>
+                    <td>{product.category}</td>
+                    <td>{product.size}</td>
+                    <td>{product.quantity}</td>
+                    <td>{product.price}</td>
+                    <td>
+                        <div className='check-cart'>
+                            <input type="number" name="" id="" />
+                            <AiOutlineShoppingCart className='cart' />
+                            <input type="checkbox" name="" id="" />
+                        </div>
+                    </td>
+                </tr>) 
+            }
+        </>
     );
 };
 
